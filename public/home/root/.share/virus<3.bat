58a5f505ec35c81f806a7f09d9e611c76d245073
Henry <henry.arcangello@proton.me>
2024-12-10 20:11:17 +0000

    add

---
+++ public/home/root/.share/virus<3.bat
@@ -0,0 +1,25 @@
+@echo off
+color 0A
+
+
+echo.
+echo        ******       ******
+echo      **    **     **    **
+echo    **        ** **        **
+echo   **          ***          **
+echo   **                       **
+echo    **                   **
+echo      **               **
+echo        **           **
+echo          **       **
+echo            **   **
+echo              **
+echo.
+
+
+echo Você é como a nota mais pura, a melodia mais linda.
+echo Você é a música que vem tocando na minha cabeça desde que te conheci naquela escola.
+echo.
+echo Eu te amo para todo sempre.
+echo.
+pause
